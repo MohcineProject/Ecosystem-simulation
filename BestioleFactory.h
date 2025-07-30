b3b12f6090aac2cce27f56e67288086975f1f362
Zahdi <ZahdiMohcine20@gmail.com>
2025-07-30 11:54:28 +0200

    Updating the bestiole.h file

--- BestioleFactory.h
+++ BestioleFactory.h
@@ -9,17 +9,43 @@
 #include "Bestiole.h"
 
 class BestioleFactory {
-
-
+        /**
+         * The singleton instance of the BestioleFactory.
+         */
         static BestioleFactory* bestioleFactory  ;
+
+        /**
+         * The aquarium instance used to retrieve the environement parameters for the creation of bestioles.
+         */
         const Aquarium* aquarium;
 
+        /**
+         * Constructor of the bestiole factory.
+         * @param aquarium the Aquarium instance used by the factory to get parameters for the creation of bestioles.
+         */
         explicit BestioleFactory(const Aquarium* aquarium);
 
-public:
-        static BestioleFactory* getBestioleFactory(const Aquarium* aquarium) ;
-        Bestiole* createBestiole(const string& bestiole_type) const;
+        /**
+         * A function used to equip the bestiole wit
+         * @param b
+         */
         void equip_bestiole(Bestiole* b) const;
+
+public:
+        /**
+         * A static function to retrieve the singleton instance of the bestiole factory.
+         * @param aquarium the Aquarium instance used to initialize the factory.
+         * @return the bestiole factory singleton.
+         */
+        static BestioleFactory* getBestioleFactory(const Aquarium* aquarium) ;
+
+        /**
+         * The function used to create a factory.
+         * @param bestiole_type The type of the bestiole to be created.
+         * @return a pointer to the newly created bestiole.
+         */
+        Bestiole* createBestiole(const string& bestiole_type) const;
+
 
 };
 
